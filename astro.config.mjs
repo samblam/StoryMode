5898a03b933af95663f53db0b971115d76d4a619
samblam <samuel.ellis.barefoot@gmail.com>
2025-01-08 22:02:32 -0500

    put vercel back in

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'static',
-  vite: {
-    build: {
-      cssCodeSplit: true,
-      rollupOptions: {
-        output: {
-          format: 'esm',
-          chunkFileNames: 'chunks/[name].[hash].mjs',
-          assetFileNames: 'assets/[name].[hash][extname]',
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          }
-        }
-      }
-    },
-    ssr: {
-      noExternal: ['howler']
-    }
-  }
+  output: 'server',
+  adapter: vercel()
 });
